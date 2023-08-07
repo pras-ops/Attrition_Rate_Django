c1b59b1ac2f8e9a9c06ede8ecc79b5ee64fde780
pras-ops <prashantjacob.bilaspur@gmail.com>
2023-08-07 14:15:53 +0530

    new file:   Notebook/Attrition Rate.ipynb 	modified:   Notebook/trial.ipynb 	deleted:    Pre_trail_file/processed table.csv 	renamed:    Pre_trail_file/mysite -> Pre_trail_file/test_mysite 	deleted:    Pre_trail_file/trial.ipynb 	new file:   artifacts/data.csv 	new file:   artifacts/test.csv 	new file:   artifacts/train.csv 	modified:   requirements.txt 	new file:   src/components/data_ingestion.py 	new file:   src/components/data_transformation.py 	new file:   src/components/model_trainer.py 	modified:   src/utils/__init__.py 	new file:   src/utils/utils.py 	modified:   Pre_trail_file/test_mysite (modified content)

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,18 +1,17 @@
 import os
 import sys
-import numpy as np 
+import numpy as np
 import pandas as pd
 import dill
 import pickle
-from sklearn.metrics import r2_score
-
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 from exception import CustomException
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
@@ -20,31 +19,30 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-
+        for model_name, model in models.items():
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            train_model_score = accuracy_score(y_train, y_train_pred)
+            test_model_score = accuracy_score(y_test, y_test_pred)
 
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = {
+                "train_accuracy": train_model_score,
+                "test_accuracy": test_model_score
+            }
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
